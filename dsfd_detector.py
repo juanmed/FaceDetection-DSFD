5ea6b19eed25004a101daeb432a90c3006661a57
juan <21977733+juanmed@users.noreply.github.com>
2020-02-21 20:17:03 +0900

    - no idea

--- dsfd_detector.py
+++ dsfd_detector.py
@@ -7,27 +7,25 @@ from utils import get_detections
 
 class DSFD_detector():
 
-	def __init__(self, conf_thresh = 0.3, target_size = (800, 800), device = 'cuda'):
+    def __init__(self, conf_thresh = 0.3, target_size = (800, 800), device = 'cuda'):
 
-		self.device = torch.device(device)
-		self.conf_thresh = conf_thresh
-		self.target_size = (800, 800)
-		self.net = SSD("test")
-		self.net.load_state_dict(torch.load('weights/WIDERFace_DSFD_RES152.pth'))
-		self.to(self.device)
+        self.device = torch.device(device)
+        self.conf_thresh = conf_thresh
+        self.target_size = (800, 800)
+        self.net = SSD("test")
+        self.net.load_state_dict(torch.load('weights/WIDERFace_DSFD_RES152.pth'))
+        self.net.to(self.device)
 
-	def detect(self, image):
-		detections = self.net.detect_on_image(image, self.target_size, self.device, is_pad=False, keep_thresh=conf_thresh)
-		bboxs = get_detections(detections, self.conf_thresh)
-		return bboxs
+    def detect(self, image):
+        detections = self.net.detect_on_image(image, self.target_size, self.device, is_pad=False, keep_thresh=self.conf_thresh)
+        bboxs = get_detections(detections, self.conf_thresh)
+        return [0,bboxs,0]
 
 if __name__ == '__main__':
-	
-	print("hola")
+    
+    img_path = '/home/fer/Videos/YDXJ2900.jpg'
 
-	img_path = '/home/fer/Videos/YDXJ2900.jpg'
-
-	detector = DSFD_detector()
-	img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-	bboxs = detector.detect(img)
-	print(bboxs)
+    detector = DSFD_detector()
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    bboxs = detector.detect(img)
+    print(bboxs)
